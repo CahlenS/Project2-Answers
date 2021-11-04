90d801a7f5336495e08118a0c486ce2a545aed92
CahlenS <CahlenSDixon@gmail.com>
2021-11-04 11:06:46 -0400

    Commit

---
+++ Hello World/src/MyPackage/MyThing.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+public class MyThing {
+	
+	int myWholeNumber; //Short for integer, 32 bit, whole #
+	float myDecimal; //'Floating Point'/'Decimal #, 64 bit, inaccurate
+	double myMorePreciseDecimal; // 128 bits, double precision decimal #
+	
+	short mySmallNumber = 5;
+
+}
